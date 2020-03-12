f41902c10b64ce6c33e8480faaf4600087de35cb
Chao Zhang <chao.zhang@math.tu-chemnitz.de>
2020-03-12 18:59:51 +0100

    add nordrhein-westfalen data

--- scripts/parseData.js
+++ scripts/parseData.js
@@ -195,6 +195,26 @@ d3.csv("https://raw.githubusercontent.com/ChaliZhg/corona/master/data/niedersach
     state_bubbles.push(anode);
   }
 });
+
+//read in nordrhein-westfalen data
+d3.csv("https://raw.githubusercontent.com/ChaliZhg/corona/master/data/nordrhein-westfalen.csv", function(data)
+{
+  for (var i = data.length - 1; i >= 0; i--)
+  {
+    anode =
+    {
+      latitude: data[i].Latitude,
+      longitude: data[i].Longitude,
+      fillKey: "subhigh",
+      radius: Math.sqrt(data[i].Fälle)*scale,
+      value: data[i].Fälle,
+      centered: data[i].Bundesland,
+      borderWidth: 0.5,
+    };
+    state_bubbles.push(anode);
+  }
+});
+
 var bubble_map = new Datamap(
 {
   element: document.getElementById('canvas'),
