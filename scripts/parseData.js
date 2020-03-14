5c354fd8aa906fd0cf62884023db3ca3282bc9c2
Chao Zhang <chao.zhang@math.tu-chemnitz.de>
2020-03-14 21:37:56 +0100

    update as zeit changed data type from csv to json

--- scripts/parseData.js
+++ scripts/parseData.js
@@ -9,191 +9,168 @@ national_bubbles = [];
 state_bubbles = [];
 bubbles = [];
 
-//parse zeit's national data
-d3.csv("https://interactive.zeit.de/cronjobs/2020/corona/bundeslaender.csv", function(data)
-{
-  for (var i = data.length - 1; i >= 0; i--) {
-    if(data[i].Bundesland.length>0)
+var result;
+
+
+  var bubble_map = new Datamap(
+  {
+    element: document.getElementById('canvas'),
+    scope: 'deu',
+    responsive: true,
+    geographyConfig:
     {
-      if(data[i].Bundesland=="Summe")
+      popupOnHover: true,
+      highlightOnHover: false,
+      borderColor: '#444',
+      borderWidth: 0.5,
+    },
+    fills:
+    {
+      defaultFill: "#f4f4eb",
+      high: 'rgb(166, 189, 219)', //EB3550
+      middle: '#6FE88E',
+      low: '#4D4D4D',
+      subhigh: 'rgb(166, 189, 219)', //EBA19E
+    },
+    bubblesConfig:
+    {
+      fillOpacity:0.75,
+      borderWidth: 1.5,
+      borderOpacity: 1, 
+      borderColor: '#FFFFFF',
+      popupOnHover: true,
+      radius: null,
+      popupTemplate: function(geography, data)
       {
-        total = data[i].Fälle;
-        death = data[i].Todesfälle;
-        recovery = data[i].Genesene;
-        if (death<10)
-        {
-          death = '0' + death.toString();
-        }
+        return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
+      },
+    },
+  // data: {
+  // 'Sachsen': { fillKey: 'defautlFill' },
+  // },
+  setProjection: function (element)
+  {
+    var projection = d3.geo.mercator()
+      .center([16.0, 52]) // always in [East Latitude, North Longitude]
+      .scale(2600);
+      // .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+      var path = d3.geo.path().projection(projection);
+      return { path: path, projection: projection };
       }
-      else if(data[i].Bundesland=="Letzte Änderung")
-      {
-        timeString = data[i].Fälle;
-      }
-      else
-      {
-        if (data[i].Fälle=="")
+      });
+
+function setup(argument)
+{
+  scale = 2;
+  custom_text = {};
+  url = "https://interactive.zeit.de/cronjobs/2020/corona/data.json";
+  loadJSON(url, gotContent, 'json');
+
+  function gotContent(data)
+  {
+    result = data;
+    load_states_data = data.states;
+    for (var i = load_states_data.length - 1; i >= 0; i--)
+    {
+      state = load_states_data[i].state;
+      count = (load_states_data[i].count);
+      recovered = (load_states_data[i].recovered);
+      dead = (load_states_data[i].dead);
+      count_node =
         {
-          data[i].Fälle = 0
-        }
-        infected_string = data[i].Fälle.toString();
-        infected_string_split = infected_string.split(' ');
-        infected_number = infected_string_split[0];
-        anode = 
-        {
-          centered: data[i].Bundesland,
+          centered: state,
           fillKey: "high",
-          radius: Math.sqrt(infected_number)*scale,
-          value:infected_number,
+          radius: Math.sqrt(count)*scale,
+          value: count,
           type: "2",
         };
-        national_bubbles.push(anode);
+      national_bubbles.push(count_node);
+      recovered_node =
+        {
+          centered: state,
+          fillKey: "middle",
+          radius: Math.sqrt(recovered)*scale,
+          value:recovered,
+          type: "1",
+        };
+      national_bubbles.push(recovered_node);
+      dead_node =
+        {
+          centered: state,
+          fillKey: "low",
+          radius: Math.sqrt(dead)*scale,
+          value: dead,
+          type: "0",
+        };
+      national_bubbles.push(dead_node);
+      custom_text[state] = count;
+    }
+  document.getElementById('time').innerHTML = data.changeTimestamp;
+  document.getElementById('total_number').innerHTML = data.totals.count;
+  document.getElementById('death_number').innerHTML = '0'+data.totals.dead;
+  document.getElementById('recovery_number').innerHTML = data.totals.recovered;
+  }
+  states = ['sachsen',
+            'sachsen-anhalt',
+            'hessen',
+            'bayern',
+            'baden-wuerttemberg',
+            'niedersachsen',
+            'nordrhein-westfalen',
+            'bremen',
+            'hamburg',
+            'berlin',
+            'saarland',
+            'thuringia',
+            'rheinlandpfalz',
+            'brandenburg',
+            'schleswig-holstein',
+            'mecklenburg-vorpommern'
+            ];
 
-        if (data[i].Todesfälle!='') {
+  for (var j = states.length - 1; j >= 0; j--)
+  {
+    url = "https://raw.githubusercontent.com/ChaliZhg/corona/master/data/"+states[j]+".csv";
+    d3.csv(url, function(data)
+    {
+      for (var i = data.length - 1; i >= 0; i--)
+      {
+        if (data[i].Latitude!="")
+        {
+          anode =
+          {
+            latitude: data[i].Latitude,
+            longitude: data[i].Longitude,
+            fillKey: "subhigh",
+            radius: Math.sqrt(data[i].Fälle)*scale,
+            value: parseInt(data[i].Fälle),
+            centered: data[i].Bundesland,
+            borderWidth: 0.5,
+            type: -3,
+            city: states[j],
+          };
+          state_bubbles.push(anode);
+        }
+        else
+        {
           anode =
           {
             centered: data[i].Bundesland,
-            fillKey: "low",
-            radius: Math.sqrt(data[i].Todesfälle)*scale,
-            value:data[i].Todesfälle,
-            type: "0",
-          };
-          national_bubbles.push(anode);
-        }
-        if (data[i].Genesene!='') {
-          anode =
-          {
+            fillKey: "subhigh",
+            radius: Math.sqrt(data[i].Fälle)*scale,
+            value: parseInt(data[i].Fälle),
             centered: data[i].Bundesland,
-            fillKey: "middle",
-            radius: Math.sqrt(data[i].Genesene)*scale,
-            value:data[i].Genesene,
-            type: "1",
+            borderWidth: 0.5,
+            type: -3,
           };
-          national_bubbles.push(anode);
+          state_bubbles.push(anode);
         }
-        var myString = data[i].Bundesland;
-        // custom_text[myString] = data[i].Bundesland+" ("+infected_number + ")";
-        custom_text[myString] = infected_number;
+
       }
-    }
-  };
-  document.getElementById('time').innerHTML = timeString;
-  document.getElementById('total_number').innerHTML = total;
-  document.getElementById('death_number').innerHTML = death;
-  document.getElementById('recovery_number').innerHTML = recovery;
+    });
+  }
 
-});
-
-states = ['sachsen',
-          'sachsen-anhalt',
-          'hessen',
-          'bayern',
-          'baden-wuerttemberg',
-          'niedersachsen',
-          'nordrhein-westfalen',
-          'bremen',
-          'hamburg',
-          'berlin',
-          'saarland',
-          'thuringia',
-          'rheinlandpfalz',
-          'brandenburg',
-          'schleswig-holstein',
-          'mecklenburg-vorpommern'
-          ];
-
-for (var j = states.length - 1; j >= 0; j--) {
-  url = "https://raw.githubusercontent.com/ChaliZhg/corona/master/data/"+states[j]+".csv";
-  d3.csv(url, function(data)
-  {
-    for (var i = data.length - 1; i >= 0; i--)
-    {
-      if (data[i].Latitude!="")
-      {
-        anode =
-        {
-          latitude: data[i].Latitude,
-          longitude: data[i].Longitude,
-          fillKey: "subhigh",
-          radius: Math.sqrt(data[i].Fälle)*scale,
-          value: parseInt(data[i].Fälle),
-          centered: data[i].Bundesland,
-          borderWidth: 0.5,
-          type: -3,
-          city: states[j],
-        };
-        state_bubbles.push(anode);
-      }
-      else
-      {
-        anode =
-        {
-          centered: data[i].Bundesland,
-          fillKey: "subhigh",
-          radius: Math.sqrt(data[i].Fälle)*scale,
-          value: parseInt(data[i].Fälle),
-          centered: data[i].Bundesland,
-          borderWidth: 0.5,
-          type: -3,
-        };
-        state_bubbles.push(anode);
-      }
-
-    }
-  });
-}
-
-
-var bubble_map = new Datamap(
-{
-  element: document.getElementById('canvas'),
-  scope: 'deu',
-  responsive: true,
-  geographyConfig:
-  {
-    popupOnHover: true,
-    highlightOnHover: false,
-    borderColor: '#444',
-    borderWidth: 0.5,
-  },
-  fills:
-  {
-    defaultFill: "#f4f4eb",
-    high: 'rgb(166, 189, 219)', //EB3550
-    middle: '#6FE88E',
-    low: '#4D4D4D',
-    subhigh: 'rgb(166, 189, 219)', //EBA19E
-  },
-  bubblesConfig:
-  {
-    fillOpacity:0.75,
-    borderWidth: 1.5,
-    borderOpacity: 1, 
-    borderColor: '#FFFFFF',
-    popupOnHover: true,
-    radius: null,
-    popupTemplate: function(geography, data)
-    {
-      return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
-    },
-  },
-// data: {
-// 'Sachsen': { fillKey: 'defautlFill' },
-// },
-setProjection: function (element)
-{
-  var projection = d3.geo.mercator()
-.center([16.0, 52]) // always in [East Latitude, North Longitude]
-.scale(2600);
-// .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-var path = d3.geo.path().projection(projection);
-return { path: path, projection: projection };
-}
-});
-
-
-
-//ISO ID code for city or <state></state>
-setTimeout(() => {
+  setTimeout(() => {
 
   national_bubbles.sort(function (a, b) {
     if (a.type > b.type) {
@@ -216,16 +193,19 @@ setTimeout(() => {
     return 0;
   });
 
+    // only start drawing bubbles on the map when map has rendered completely.
+    // bubble_map.labels({'customLabelText': custom_text});
+    bubble_map.bubbles(national_bubbles, {
+      popupTemplate: function (geo, data) {
+        return `<div class="hoverinfo">${data.centered}: ${data.value}</div>`;
+      }
+    });
+    bubble_map.labels({'customLabelText': custom_text, "fontSize": 40});
+  }, 1000);
 
-  // only start drawing bubbles on the map when map has rendered completely.
-  // bubble_map.labels({'customLabelText': custom_text});
-  bubble_map.bubbles(national_bubbles, {
-    popupTemplate: function (geo, data) {
-      return `<div class="hoverinfo">${data.centered}: ${data.value}</div>`;
-    }
-  });
-  bubble_map.labels({'customLabelText': custom_text, "fontSize": 40});
-}, 1500);
+
+
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   var checkbox = document.querySelector('input[type="checkbox"]');
