8bcd6f3caf4eaf86d9d69725b91721d15d3c2185
Chao Zhang <chao.zhang@math.tu-chemnitz.de>
2020-03-29 15:14:00 +0200

    update trend

--- scripts/trend.js
+++ scripts/trend.js
@@ -437,7 +437,7 @@ function makeChart (data, markers) {
 }
 
 var parseDate  = d3.time.format('%Y-%m-%d').parse;
-d3.json('https://gist.githubusercontent.com/ChaliZhg/afff054c4e46e1ae1caf62a3e59e6df4/raw/4ca7decc23b07f5215fbf6408841bc522594aaaa/csvjson.json', function (error, rawData) {
+d3.json('https://gist.githubusercontent.com/ChaliZhg/afff054c4e46e1ae1caf62a3e59e6df4/raw/b3da4dba3bdc351fca77cb7c1d282f33e443b547/csvjson.json', function (error, rawData) {
   if (error) {
     console.error(error);
     return;
